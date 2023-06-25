987dec87043e60eabf2d83316e1594b74b8caf2e
JavaScriptContinueForever <97775646+AhmedWithJSDevelopment@users.noreply.github.com>
2023-06-25 10:15:11 +0300

    Add files via upload

--- loaadmmore.js
+++ loaadmmore.js
@@ -1,14 +1,14 @@
   let loadMoreBtn = document.querySelector('#load-more');
-  let currentItem = 3;
+  let currentItemL = 3;
 
   loadMoreBtn.onclick = () => {
       let boxes = [...document.querySelectorAll('.span6 .accordion .accordion-item')];
-      for (var i = currentItem; i < currentItem + 3; i++) {
+      for (var i = currentItemL; i < currentItemL + 3; i++) {
           boxes[i].style.display = 'inline-block';
       }
-      currentItem += 3;
+      currentItemL += 3;
 
-      if (currentItem >= boxes.length) {
+      if (currentItemL >= boxes.length) {
           loadMoreBtn.style.display = 'none';
 
 
